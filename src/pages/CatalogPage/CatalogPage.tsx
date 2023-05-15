3dabfe056c918bfb4c28f58eac1e0879efa49598
Rekonir <zhed-nikita@mail.ru>
2023-05-15 18:29:44 +0300

    add filter by ganre

--- src/pages/CatalogPage/CatalogPage.tsx
+++ src/pages/CatalogPage/CatalogPage.tsx
@@ -1,17 +1,32 @@
 import FilmData from '../../FilmData.json'
 import styles from './CatalogPage.module.scss'
 import Button from '../../components/UI/Button/Button';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import Path from '../../components/UI/Path/Path';
 import FilmsList from '../../components/FilmsList/FilmsList';
 import Selector from '../../components/UI/Selector/Selector';
 import { useEffect, useState } from 'react';
+import GenresData from '../../GenresData.json'
+
 
 
 const CatalogPage = () => {
     const [Films, setFilms] = useState(FilmData);
     const [sortState, setSort] = useState('none');
+    const { genre } = useParams()
+    useEffect(() => {
+        const genres = genre?.split('+')
+        const filterFilms = FilmData.filter(film => {
+            return film.genre.some(genre => {
+                return genres?.includes(genre.name_en);
+            });
+        });
+        if (filterFilms.length !== 0) {
+            setFilms(filterFilms)
+        }
+        else { setFilms(FilmData) }
 
+    }, [genre])
     useEffect(() => {
         switch (sortState) {
             case 'по рейтингу':
@@ -42,15 +57,9 @@ const CatalogPage = () => {
             </Path>
             <h1 className={styles.header} > Фильмы смотреть онлайн </h1>
             <div className={styles.filtersBox}>
-                <Button variant={'outlined'}>
-                    Жанр
-                    <img src="https://start.ru/static/images/product/arrow-down.svg" alt="" />
-                </Button>
-                <Button variant={'outlined'}>
-                    Год
-                    <img src="https://start.ru/static/images/product/arrow-down.svg" alt="" />
-                </Button>
-                <Selector name={"Сортировка"} filter={false} setSort={setSort} array={['по количеству оценок на кинопоиске', 'по рейтингу', 'по дате выхода (сначала свежие)', 'по дате выхода (сначала старые)', 'по алфавиту (А-Я)', 'по алфавиту (Я-А)']} />
+                <Selector name={'Жанр'} array={GenresData} filter={'genre'} />
+                <Selector name={'Год'} array={['2023', '2015']} filter={'year'} />
+                <Selector name={"Сортировка"} filter='none' setSort={setSort} array={['по количеству оценок на кинопоиске', 'по рейтингу', 'по дате выхода (сначала свежие)', 'по дате выхода (сначала старые)', 'по алфавиту (А-Я)', 'по алфавиту (Я-А)']} />
             </div >
 
             <FilmsList films={Films} ></FilmsList>
