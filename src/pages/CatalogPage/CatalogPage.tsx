7fb2351f2063c891dc246744bd0727b3272b0f5c
Rekonir <zhed-nikita@mail.ru>
2023-05-16 16:46:53 +0300

    work with filters and url

--- src/pages/CatalogPage/CatalogPage.tsx
+++ src/pages/CatalogPage/CatalogPage.tsx
@@ -7,48 +7,56 @@ import FilmsList from '../../components/FilmsList/FilmsList';
 import Selector from '../../components/UI/Selector/Selector';
 import { useEffect, useState } from 'react';
 import GenresData from '../../GenresData.json'
+import YearData from '../../YearData.json'
+
 
 
 
 const CatalogPage = () => {
     const [Films, setFilms] = useState(FilmData);
     const [sortState, setSort] = useState('none');
+    const [yearFilter, setYearFilter] = useState<string>('')
     const { genre } = useParams()
     useEffect(() => {
-        const genres = genre?.split('+')
-        const filterFilms = FilmData.filter(film => {
-            return film.genre.some(genre => {
-                return genres?.includes(genre.name_en);
-            });
-        });
-        if (filterFilms.length !== 0) {
-            setFilms(filterFilms)
+        let filterFilms = FilmData;
+        if (yearFilter) {
+            filterFilms = filterFilms.filter(film => String(film.year) === yearFilter.toLowerCase());
         }
-        else { setFilms(FilmData) }
 
-    }, [genre])
+        if (genre) {
+            const genres = genre.split('+')
+            filterFilms = filterFilms.filter(film => {
+                return film.genre.some(genre => {
+                    return genres.includes(genre.name_en);
+                });
+            });
+        }
+        setFilms(filterFilms)
+    }, [yearFilter, genre])
+
     useEffect(() => {
         switch (sortState) {
             case 'по рейтингу':
-                setFilms([...Films].sort((a, b) => b.rating - a.rating));
+                setFilms(prev => [...prev].sort((a, b) => b.rating - a.rating));
                 break;
             case 'по дате выхода (сначала свежие)':
-                setFilms([...Films].sort((a, b) => b.year - a.year));
+                setFilms(prev => [...prev].sort((a, b) => b.year - a.year));
                 break;
             case 'по дате выхода (сначала старые)':
-                setFilms([...Films].sort((a, b) => a.year - b.year));
+                setFilms(prev => [...prev].sort((a, b) => a.year - b.year));
                 break;
             case 'по алфавиту (А-Я)':
-                setFilms([...Films].sort((a, b) => a.name_ru.localeCompare(b.name_ru)));
+                setFilms(prev => [...prev].sort((a, b) => a.name_ru.localeCompare(b.name_ru)));
                 break;
             case 'по алфавиту (Я-А)':
-                setFilms([...Films].sort((a, b) => b.name_ru.localeCompare(a.name_ru)));
+                setFilms(prev => [...prev].sort((a, b) => b.name_ru.localeCompare(a.name_ru)));
                 break;
             default:
                 setFilms(FilmData);
                 break;
         }
     }, [sortState]);
+
     return (
         <div className={styles.page}>
             <Path>
@@ -58,19 +66,16 @@ const CatalogPage = () => {
             <h1 className={styles.header} > Фильмы смотреть онлайн </h1>
             <div className={styles.filtersBox}>
                 <Selector name={'Жанр'} array={GenresData} filter={'genre'} />
-                <Selector name={'Год'} array={['2023', '2015']} filter={'year'} />
+                <Selector name={'Год'} array={YearData} filter={'year'} setYearFilter={setYearFilter} />
+                <Button variant='outlined'>Сбросить</Button>
                 <Selector name={"Сортировка"} filter='none' setSort={setSort} array={['по количеству оценок на кинопоиске', 'по рейтингу', 'по дате выхода (сначала свежие)', 'по дате выхода (сначала старые)', 'по алфавиту (А-Я)', 'по алфавиту (Я-А)']} />
             </div >
-
-            <FilmsList films={Films} ></FilmsList>
-
+            <FilmsList films={Films}></FilmsList>
             <Button variant='outlined'>
                 Показать ещё
                 <img src="https://start.ru/static/images/product/arrow-down.svg" alt="" />
             </Button>
         </div >
-
-
     );
 };
 
